2e3efdb7ecf7c00e813228b2f2737bbe93bb74e0
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-04-23 18:12:07 +0300

    router for store

--- naples/routes/__init__.py
+++ naples/routes/__init__.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Request
 from .user import user_router
 
 from .auth import router as auth_router
+from .store import store_router
 
 # from .notify import notification_test_router
 
@@ -12,6 +13,7 @@ router = APIRouter(prefix="/api", tags=["API"])
 
 router.include_router(user_router)
 router.include_router(auth_router)
+router.include_router(store_router)
 
 
 @router.get("/list-endpoints/")
---
+++ naples/routes/store.py
@@ -0,0 +1,65 @@
+from typing import Sequence, cast
+from fastapi import Depends, APIRouter, status, HTTPException
+
+import naples.models as m
+import naples.schemas as s
+from naples.logger import log
+
+import sqlalchemy as sa
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import Executable
+
+from naples.dependency import get_current_user
+from naples.database import get_db
+
+
+store_router = APIRouter(prefix="/stores", tags=["Stores"])
+
+
+@store_router.get(
+    "/{store_uuid}",
+    status_code=status.HTTP_200_OK,
+    response_model=s.StoreOut,
+    responses={
+        404: {"description": "Store not found"},
+    },
+)
+def get_store(
+    store_uuid: str,
+    db: Session = Depends(get_db),
+    current_user: m.User = Depends(get_current_user),
+):
+    """Returns the store"""
+
+    store: m.Store | None = db.scalar(sa.select(m.Store).where(m.Store.uuid == store_uuid))
+    if not store:
+        log(log.ERROR, "Store [%s] not found", store_uuid)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Store not found")
+    return store
+
+
+# TODO: need to implement this route or it must be without current_user
+@store_router.get("/", status_code=status.HTTP_200_OK, response_model=s.Stores)
+def get_stors(
+    db: Session = Depends(get_db),
+    current_user: m.User = Depends(get_current_user),
+):
+    query = sa.select(m.Store)
+    stores: Sequence[m.Store] = db.scalars(query).all()
+    return s.Stores(stores=cast(list, stores))
+
+
+@store_router.post("/", status_code=status.HTTP_201_CREATED, response_model=s.StoreOut)
+def create_stote(
+    job: s.Store,
+    db: Session = Depends(get_db),
+    current_user: m.User = Depends(get_current_user),
+):
+    new_store: m.Store = m.Store(
+        **job.model_dump(),
+        user_id=current_user.id,
+    )
+    db.add(new_store)
+    db.commit()
+    log(log.INFO, "Created store [%s] for user [%s]", new_store.name, new_store.user_id)
+    return new_store
