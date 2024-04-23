bf8b934b32335558b48848b93531e73254b16d88
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-04-23 18:11:50 +0300

    test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,14 +1,33 @@
+import uuid
 from pydantic import BaseModel
 
 
 class TestUser(BaseModel):
     __test__ = False
 
+    id: int
     email: str
     password: str
+
+
+class TestStore(BaseModel):
+    __test__ = False
+
+    uuid: str
+    name: str
+    header: str
+    sub_header: str
+    url: str
+    logo_url: str
+    about_us: str
+    email: str
+    instagram_url: str
+    messenger_url: str
+    user_id: int
 
 
 class TestData(BaseModel):
     __test__ = False
 
     test_users: list[TestUser]
+    test_stores: list[TestStore]
