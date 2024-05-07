a089230d6a8ba915b29c2951a854c09b8384c014
Taraslysak <lysak.ipr@gmail.com>
2024-05-07 11:36:57 +0300

    chore: Update test_booked_dates.py to fix date handling

--- tests/test_booked_dates.py
+++ tests/test_booked_dates.py
@@ -15,7 +15,7 @@ def test_created_booked_dates(client: TestClient, full_db: Session, headers: dic
     item = full_db.scalar(select(m.Item))
     assert item
 
-    date = datetime.now(UTC)
+    date = datetime.now()
 
     req_payload = s.BookedDatesBatchIn(item_uuid=item.uuid, dates=[date])
 
@@ -23,19 +23,33 @@ def test_created_booked_dates(client: TestClient, full_db: Session, headers: dic
 
     assert res.status_code == 201
 
-    item_res = client.get(f"/api/items/{item.uuid}", params={"store_uuid": store.uuid})
+    item_res = client.get(f"/api/items/{item.uuid}", params={"store_url": store.url})
 
     assert item_res.status_code == 200
 
-    item_data = s.ItemDetailsOut.model_validate(item_res.content)
+    item_data = s.ItemDetailsOut.model_validate(item_res.json())
 
     assert item_data.booked_dates
 
-    assert item_data.booked_dates[0].date == date
+    assert item_data.booked_dates[0] == date
 
 
 def test_get_booked_dates(client: TestClient, full_db: Session, headers: dict[str, str]):
-    pass
+    item = full_db.scalar(select(m.Item))
+    assert item
+
+    booked_date_one = m.BookedDate(date=datetime.now(), item_id=item.id)
+    booked_date_two = m.BookedDate(date=datetime.now(), item_id=item.id)
+    full_db.add([booked_date_one, booked_date_two])
+    full_db.commit()
+
+    res = client.get(f"/api/booked_dates/{item.uuid}", headers=headers)
+    assert res.status_code == 200
+
+    booked_dates = s.BookedDateListOut.model_validate(res.json())
+    assert len(booked_dates.items) == 2
+
+    assert booked_dates.items[0].date == booked_date_one.date
 
 
 def test_delete_booked_dates(client: TestClient, full_db: Session, headers: dict[str, str]):
