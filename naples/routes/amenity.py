fafaefb9a57eecc2c72b51611ed72f27539404b1
Taraslysak <lysak.ipr@gmail.com>
2024-05-16 14:58:25 +0300

    chore: Add endpoint to retrieve item amenities

--- naples/routes/amenity.py
+++ naples/routes/amenity.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from naples.database import get_db
+from naples.dependency.get_user_store import get_current_user_store
 from naples.dependency.user import get_current_user
 from naples.logger import log
 from naples import schemas as s, models as m
@@ -34,6 +35,25 @@ def create_amenity(
     db.commit()
     db.refresh(amenity)
     return amenity
+
+
+@amenities_router.get("/{item_uuid}", response_model=s.AmenitiesListOut, status_code=status.HTTP_200_OK)
+def get_item_amenities(
+    item_uuid: str,
+    db: Session = Depends(get_db),
+    current_store: m.Store = Depends(get_current_user_store),
+):
+    log(log.INFO, "User {%s} is fetching all amenities for item {%s}", current_store.uuid, item_uuid)
+    item = db.scalar(sa.select(m.Item).where(m.Item.uuid == item_uuid))
+    if not item:
+        log(log.ERROR, "Item {%s} not found", item_uuid)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+
+    if item.store_id != current_store.id:
+        log(log.ERROR, "Item {%s} does not belong to store {%s}", item_uuid, current_store.uuid)
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Item does not belong to store")
+
+    return s.AmenitiesListOut(items=[a for a in item._amenities if not a.is_deleted])
 
 
 @amenities_router.delete("/{amenity_uuid}", status_code=status.HTTP_204_NO_CONTENT)
