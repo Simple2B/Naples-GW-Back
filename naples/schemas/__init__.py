6babc88f7fcaa5ab82b84701a622a84d66679e4f
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-05-27 18:01:33 +0300

    about us info for store

--- naples/schemas/__init__.py
+++ naples/schemas/__init__.py
@@ -2,7 +2,7 @@
 from .test_data import TestUser, TestItem, TestData, TestStore, TestMember
 from .user import UserRole, UserSignIn, BaseUser, User, Users, EmailContent
 from .token import Token, TokenData, TokenOut, Auth
-from .store import Store, StoreIn, StoreOut, Stores, StoreUpdateIn
+from .store import Store, StoreIn, StoreAboutUsDescription, StoreAboutUs, StoreOut, Stores, StoreUpdateIn
 from .item import (
     ItemStage,
     ItemIn,
