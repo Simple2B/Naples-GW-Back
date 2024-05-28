71420288c82b744ba2c23acead97fe039757b2e1
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-05-28 09:58:51 +0300

    test

--- tests/test_users.py
+++ tests/test_users.py
@@ -28,3 +28,28 @@ def test_get_users(
     assert response.status_code == 200
     users_out = s.Users.model_validate(response.json())
     assert len(users_out.users) == len(test_data.test_users)
+
+
+def test_update_user(
+    client: TestClient,
+    db: Session,
+    headers: dict[str, str],
+    test_data: s.TestData,
+):
+    user = test_data.test_users[0]
+    phone = "1234567890"
+    user_update = s.UserUpdate(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        role=user.role,
+        phone=phone,
+    )
+    response = client.patch(
+        "/api/users/",
+        headers=headers,
+        json=user_update.model_dump(),
+    )
+    assert response.status_code == 200
+    users_out = s.Users.model_validate(response.json())
+    assert users_out
