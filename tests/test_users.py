22d0e4844cef57ab77131db83a63cfe57bbfb68d
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-06-03 11:17:14 +0300

    test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,7 @@
 import base64
 from fastapi.testclient import TestClient
 from moto import mock_aws
+from mypy_boto3_s3 import S3Client
 from mypy_boto3_ses import SESClient
 import sqlalchemy as sa
 
@@ -82,6 +83,36 @@ def test_update_user(
     assert db_user.phone == phone
     assert db_user.last_name == "last_name"
     assert db_user.first_name == user.first_name
+
+
+def test_upload_avatar(client: TestClient, headers: dict[str, str], full_db: Session, s3_client: S3Client):
+    with open("tests/house_example.png", "rb") as f:
+        user_model = full_db.scalar(sa.select(m.User))
+        assert user_model
+
+        response = client.post(
+            "/api/users/avatar",
+            headers=headers,
+            files={"avatar": ("test.jpg", f, "image/jpeg")},
+        )
+        assert response.status_code == 201
+
+        user_res = client.get("/api/users/me", headers=headers)
+        assert user_res.status_code == 200
+
+        user = s.User.model_validate(user_res.json())
+
+        assert user.avatar and user.avatar.url == user_model.avatar.url
+
+        full_db.refresh(user_model)
+
+        bucket_file = s3_client.get_object(
+            Bucket=CFG.AWS_S3_BUCKET_NAME,
+            Key=user_model.avatar.key,
+        )
+
+        assert bucket_file["ResponseMetadata"]["HTTPStatusCode"] == 200
+        assert bucket_file["ContentLength"] > 0
 
 
 @mock_aws
