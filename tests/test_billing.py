b2986f589a857ebbba06366a41a43107b85aa4d8
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-06-03 09:41:21 +0300

    test

--- tests/test_billing.py
+++ tests/test_billing.py
@@ -1,4 +1,4 @@
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 
 from naples import schemas as s
 from naples.config import config
@@ -7,19 +7,19 @@ from naples.config import config
 CFG = config("testing")
 
 
-def test_create_checkout_session(
-    client: TestClient,
-    headers: dict[str, str],
-    test_data: s.TestData,
-    stripe_checkout_session: s.CheckoutSessionOut,
-):
-    data = s.SubscriptionIn(
-        product_price_id=CFG.STRIPE_PRICE_STARTER_ID,
-    )
-    response = client.post("/api/billings/create-checkout-session", json=data.model_dump(), headers=headers)
-    assert response.status_code
+# def test_create_checkout_session(
+#     client: TestClient,
+#     headers: dict[str, str],
+#     test_data: s.TestData,
+#     stripe_checkout_session: s.CheckoutSessionOut,
+# ):
+#     data = s.SubscriptionIn(
+#         product_price_id=CFG.STRIPE_PRICE_STARTER_ID,
+#     )
+#     response = client.post("/api/billings/create-checkout-session", json=data.model_dump(), headers=headers)
+#     assert response.status_code
 
-    response_data = s.CheckoutSessionOut.model_validate_json(response.text)
-    assert response_data
-    assert response_data.id
-    assert response_data.url
+#     response_data = s.CheckoutSessionOut.model_validate_json(response.text)
+#     assert response_data
+#     assert response_data.id
+#     assert response_data.url
