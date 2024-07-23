9bee03eda6c56aa02fbee6dc85e2d15200a9f696
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2024-07-23 12:35:04 +0300

    subscribe

--- naples/dependency/get_user_subscribe.py
+++ naples/dependency/get_user_subscribe.py
@@ -12,11 +12,15 @@ def get_user_subscribe(store: m.Store = Depends(get_current_store), db: Session 
     """Get the current subscription for authorized user.
     Should be used for endpoints related to subscription management by clients."""
 
-    if not store.user.subscription:
+    if not store.user.subscription and store.is_protected is False:
         log(log.INFO, "User {%s} does not have a subscription", store.user.uuid)
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not have a subscription")
 
-    if store.status.value == s.StoreStatus.INACTIVE.value and store.user.role != s.UserRole.ADMIN.value:
+    if (
+        store.status.value == s.StoreStatus.INACTIVE.value
+        and store.user.role != s.UserRole.ADMIN.value
+        and store.is_protected is False
+    ):
         log(log.INFO, "Store {%s} is inactive", store.uuid)
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Subscription is expired")
 
